6b830f3148f6b7e95f6af69ec035bd311f82ded8
sergii <servasylenko@deloitte.com>
2021-04-14 08:34:45 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folderPath = @"C:\Users\Sergii\Desktop\CleanFolder";
+            if (Directory.Exists(folderPath))
+            {
+                var directory = new DirectoryInfo(folderPath);
+                RemoveFiles(directory);
+            }
+            else
+            {
+                Console.WriteLine("Directory does not found!!!");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void RemoveFiles(DirectoryInfo directory)
+        {
+            try
+            {
+                var files = directory.GetFiles();
+                foreach (var file in files)
+                {
+                    if (file.LastAccessTime < DateTime.Now.AddMinutes(-30))
+                    {
+                        file.Delete();
+                    }
+                }
+
+                var directories = directory.GetDirectories();
+                foreach (var dir in directories)
+                {
+                    RemoveFiles(dir);
+                    if (dir.LastAccessTime < DateTime.Now.AddMinutes(-30))
+                    {
+                        dir.Delete(false);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erroe {ex.Message}");
+            }
+        }
+    }
+}
